8bd714419ed25c88d1155eea2a1f4c71d69a207e
Shiveshr140 <114570394+Shiveshr140@users.noreply.github.com>
2024-07-16 23:54:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+import jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router('data/questions.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
